88e3e31b2d1b9fc16dcfc635dc7675c03f386543
Marcos <marcosnavarromartin@gmail.com>
2022-04-20 13:20:21 -0700

    First Push

---
+++ EjemploGit/EjemploGit/EjemploGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  EjemploGitApp.swift
+//  EjemploGit
+//
+//  Created by Alumno Swift on 20/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct EjemploGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
